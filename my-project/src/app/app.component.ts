766649b2d3111eca17ee1aa153fcab95c991c363
LLima888 <leandrolimajc@gmail.com>
2025-10-25 17:22:53 -0300

    upgrade to angular 19

--- my-project/src/app/app.component.ts
+++ my-project/src/app/app.component.ts
@@ -7,7 +7,6 @@ import { TasksComponent } from "./tasks/tasks.component";
 
 @Component({
     selector: 'app-root',
-    standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
     imports: [HeaderComponent, UserComponent, TasksComponent]
--- my-project/src/app/tasks/tasks.component.ts
+++ my-project/src/app/tasks/tasks.component.ts
@@ -5,11 +5,10 @@ import { NewTaskComponent } from './new-task/new-task.component';
 import { TasksService } from './tasks.service';
 
 @Component({
-  selector: 'app-tasks',
-  standalone: true,
-  templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.css',
-  imports: [TaskComponent, NewTaskComponent],
+    selector: 'app-tasks',
+    templateUrl: './tasks.component.html',
+    styleUrl: './tasks.component.css',
+    imports: [TaskComponent, NewTaskComponent]
 })
 export class TasksComponent {
   @Input({ required: true }) userId!: string;
