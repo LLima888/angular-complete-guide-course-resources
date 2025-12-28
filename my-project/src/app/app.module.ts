d3a70c0d3e44457b604aaf247e84bcf939ca677a
LLima888 <leandrolimajc@gmail.com>
2025-12-28 19:12:59 -0300

    set ng for static analysis

--- my-project/src/app/app.module.ts
+++ my-project/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser'; // provides collections of services and directives needed to run the app in a browser
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
